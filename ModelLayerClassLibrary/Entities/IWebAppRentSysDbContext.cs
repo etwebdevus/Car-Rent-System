a1033867c5496b0153e3539e1aa54827d2349e89
Hartur Barreto Brito <harturbarreto91@gmail.com>
2016-02-19 00:01:57 -0300

    NuGet Bug Fixed
    Some NuGet packages had to be restored

--- ModelLayerClassLibrary/Entities/IWebAppRentSysDbContext.cs
+++ ModelLayerClassLibrary/Entities/IWebAppRentSysDbContext.cs
@@ -1,10 +1,6 @@
 ﻿using ModelLayerClassLibrary.Abstract;
 using System;
-using System.Collections.Generic;
 using System.Data.Entity;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace ModelLayerClassLibrary.Entities
 {
